063ed90ed1405a11e88c0445040554ad5a93568c
Piyush <bhangalepiyush@gmail.com>
2022-03-15 21:03:29 +0530

    working api

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ import express from "express";
 import cors from "cors";
 
 import "./lib/bot";
+import StatsRoute from "./routes/StatsRoute";
 
 const app = express();
 
 app.use(cors());
+app.use("/stats", StatsRoute)
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`🚀 Server ready at: ${process.env.PORT || 3000}`)
--- src/routes/StatsRoute.ts
+++ src/routes/StatsRoute.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import { prisma } from "../lib/prisma";
 
 const router = Router();
 
-router.get("/stats", async (req, res) => {
-    res.json({ hello: "world" })
+router.get("/", async (req, res) => {
+    const stats = await prisma.nodes.findMany({
+        include: {
+            stats: {
+                take: 100
+            }
+        }
+    });
+
+    res.json(JSON.parse(JSON.stringify(stats, (key, value) =>
+        typeof value === 'bigint'
+            ? value.toString()
+            : value
+    )))
+
     return
 })
 
